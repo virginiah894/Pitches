8810f75d2861c11a206f792da02ce7250168eba9
virginiah894 <virgyperry@gmail.com>
2019-11-23 14:39:45 +0300

    Import migrations  and add class roles

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 from app import create_pitch,db
-from app.models import User
+from app.models import User,Role
 from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_pitch('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Role = Role)
 if __name__ == '__main__':
     manager.run()
 
