58b916900b809be1fc4687ba07e0001312e81694
virginiah894 <virgyperry@gmail.com>
2019-11-24 13:23:24 +0300

    add email configurations

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ class  Config:
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Perry@localhost/pitches'
   UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS =True
+  MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+  MAIL_PASSOWRD =os.environ.get("MAIL_PASSWORD")
+
 class prodConfig(Config):
   pass
 class DevConfig(Config):
