8093c04f91d96086ac61d15b4052f2ad857416e7
virginiah894 <virgyperry@gmail.com>
2019-11-24 16:34:11 +0300

    add tests configurations

--- config.py
+++ config.py
@@ -8,14 +8,21 @@ class  Config:
   MAIL_USE_TLS =True
   MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
   MAIL_PASSOWRD =os.environ.get("MAIL_PASSWORD")
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Perry@localhost/pitches_tests'
+
 
 class prodConfig(Config):
   pass
 class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Perry@localhost/pitches'
+
   DEBUG = True
+
   
 config_options ={
     'development':DevConfig,
     
-    'production':prodConfig
+    'production':prodConfig,
+    'test':TestConfig
   }
