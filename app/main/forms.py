d78bc0d8c5f5f3dbb700415fe26494129da0c9ff
virginiah894 <virgyperry@gmail.com>
2019-11-24 15:25:08 +0300

    add a form for users to comment on pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+class CommentForm(FlaskForm):
+  title = StringField('Comment title',validators=[Required()])
+  comment = TextAreaField('Pitch Comment', validators=[Required()])
+  submit = SubmitField('Submit')
