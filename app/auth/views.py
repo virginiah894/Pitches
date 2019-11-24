b0d5cb31f476144b6e3210b234db00522558461f
virginiah894 <virgyperry@gmail.com>
2019-11-24 10:00:20 +0300

    add routes and display features for the  login and sign up page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
-from flask import render_template
+
 from . import auth
 from flask import render_template,redirect,url_for,flash,request
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
 
@@ -10,7 +10,15 @@ from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html')
+  login_form = LoginForm()
+  if login_form.validate_on_submit():
+    user = User.query.filter_by(email = login_form.password.data).first()
+    if user is not None and user.verify_password(login_form.password.data):
+      login_user(user,login_form.remember.data)
+      return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid Username or Password')
+  title = "Pitches.com Login "   
+  return render_template('auth/login.html' login_form =login_form,title = title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
