75ab753c722478994b08d8d29cdf0131be32c716
virginiah894 <virgyperry@gmail.com>
2019-11-24 12:10:40 +0300

    addd update profile function

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,30 @@
 from flask_login import login_required
 from flask import render_template,request,redirect,url_for,abort
 from ..models import User
+from ..import db
+from .forms import UpdateProfile
 @main.route('/')
 def index():
-  @login_required
 
-  return render_template('index.html')
-@main.route('/user/<usname>')
-def profile(usname):
+
+    return render_template('index.html')
+
+@main.route('/user/<usname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(usname):
     user = User.query.filter_by(username = usname).first()
 
     if user is None:
         abort(404)
+form = UpdateProfile()
 
-    return render_template("profile/profile.html", user = user)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',usname=user.username))
+
+    return render_template('profile/update.html',form =form)
+    
