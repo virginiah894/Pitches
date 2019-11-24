42082f2ed512c6c4c7d78b6a7aa24f846633e03b
virginiah894 <virgyperry@gmail.com>
2019-11-24 09:57:44 +0300

    add a load_user  function that gets data from  database

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
-@login_manager
+@login_manager.user_loader
 def load_user(user_id):
   return User.query.get(int(user_id))
 
