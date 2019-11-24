1d31894432c619de20de0c2d20df07fe6703b962
virginiah894 <virgyperry@gmail.com>
2019-11-24 11:44:57 +0300

    Add route to link the current user if they exist or show error if they dont exist in the database

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,16 @@
 from flask_login import login_required
-
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 @main.route('/')
 def index():
   @login_required
 
   return render_template('index.html')
+@main.route('/user/<usname>')
+def profile(usname):
+    user = User.query.filter_by(username = usname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
