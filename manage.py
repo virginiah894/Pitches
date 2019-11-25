c83c9280297497a461fb7a1534d5ac671c9160c2
virginiah894 <virgyperry@gmail.com>
2019-11-25 11:10:31 +0300

    add secret key

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 import os
-from app import create_pitch,db
+from app import create_app,db
 from app.models import User,Role,Comment
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 
-app = create_pitch('test')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
